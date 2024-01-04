995fd5d285d471d8edc036226f22559458ce91ca
JustMarfix <justmarfix@vk.com>
2024-01-04 22:31:03 +0300

    Добавьте файлы проекта.

---
+++ Config.cs
@@ -0,0 +1,23 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace DoorsExplosionResistance
+{
+    public class Config : IConfig
+    {
+        [Description("Is plugin enabled? (bool)")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Is debug enabled? (bool)")]
+        public bool Debug { get; set; } = false;
+
+        [Description("List of unexplosive doors (List<DoorType>)")]
+
+        public List<DoorType> UnexplosiveDoors { get; set; } = new List<DoorType>()
+        {
+            DoorType.HID
+        };
+    }
+}
---
+++ DoorsExplosionResistance.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerHandler = Exiled.Events.Handlers.Player;
+
+namespace DoorsExplosionResistance
+{
+    public class DoorsExplosionResistance : Plugin<Config>
+    {
+        public static DoorsExplosionResistance Instance { get; set; } = new DoorsExplosionResistance();
+
+        public DoorsExplosionResistance() { }
+
+        public override string Author => "JustMarfix";
+        public override string Name => "DoorsExplosionResistance";
+
+        public override void OnEnabled()
+        {
+            PlayerHandler.DamagingDoor += OnDamagingDoor;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            PlayerHandler.DamagingDoor -= OnDamagingDoor;
+            base.OnDisabled();
+        }
+
+        public void OnDamagingDoor(DamagingDoorEventArgs ev)
+        {
+            if (Config.UnexplosiveDoors.Contains(ev.Door.Type) && ev.DamageType == Interactables.Interobjects.DoorUtils.DoorDamageType.Grenade)
+            {
+                ev.IsAllowed = false;
+                return;
+            }
+        }
+    }
+}
